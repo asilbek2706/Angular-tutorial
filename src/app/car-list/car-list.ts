6743f5bc3fd9be05b76679c1c2f7299ba5e0653c
Asilbek <asilbekkaromatov2@gmail.com>
2026-05-25 23:02:06 +0500

    Two way binding

--- src/app/car-list/car-list.ts
+++ src/app/car-list/car-list.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
 import { Car } from '../models/car';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-car-list',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './car-list.html',
   styleUrl: './car-list.css',
 })
 export class CarList {
   carName: string = 'Chevrolet Camaro';
+  carYear: Date = new Date('');
   car: Car = {
     id: 1,
     name: 'Chevrolet Camaro',
     year: new Date(2020, 5, 15),
   };
+
+  onSubmit() {
+    console.log('Form submitted with values:');
+    console.log('Car Name:', this.carName);
+    console.log('Car Year:', this.carYear);
+  }
 }
