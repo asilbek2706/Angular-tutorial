0abf62feb11886a5d15310325f8fff39e52bec9b
Asilbek <asilbekkaromatov2@gmail.com>
2026-05-25 21:28:34 +0500

    components explanation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CarList } from './car-list/car-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  template: `<app-car-list />`,
+  imports: [CarList],
 })
 export class App {
   protected readonly title = signal('angular-course');
